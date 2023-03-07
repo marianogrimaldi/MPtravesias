95acae82510677064fc93b0e5b04baabf8adbdf1
marianogrimaldi <marianogdanielgrimaldi@gmail.com>
2023-03-06 22:58:49 -0300

    mp travesias idioma ing-esp

---
+++ traductor.js
@@ -0,0 +1,48 @@
+const flagsElement = document.getElementById("flags");
+
+const textsToChange = document.querySelectorAll("[data-section]");
+
+const changeLanguage = async  language=>{
+    const requestJson = await fetch(`/languages/${language}.json`)
+    const texts = await requestJson.json()
+
+    for(const textToChange of textsToChange){
+       const section = textToChange.dataset.section
+       const value = textToChange.dataset.value
+      
+
+       textToChange.innerHTML = texts [section][value]
+    }
+}
+
+flagsElement.addEventListener("click", (e) => {
+    changeLanguage(e.target.parentElement.dataset.language) 
+})
+
+
+
+
+
+let idiomaIngles = document.getElementById ("in")
+let idiomaEspañol = document.getElementById ("es")
+
+
+//CODIGO PARA QUE QUEDE GUARDADO EL IDIOMA EN LOCAL STORAGE
+
+let ingles = localStorage.getItem("ing")
+
+if(ingles == "true"){
+    document.body.classList.add("ingles")
+}else{
+    document.body.classList.remove("ingles")
+}
+
+idiomaIngles.addEventListener("click", ()=>{
+    document.body.classList.add("ingles")
+    localStorage.setItem("ing",true)
+})
+
+idiomaEspañol.addEventListener("click", ()=>{
+    document.body.classList.remove("ingles")
+    localStorage.setItem("ing", false)
+})
