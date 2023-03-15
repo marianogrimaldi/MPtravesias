b6dcf6b5cec9fc8603b14c83e7afaaf66f663477
Martín Cabo <mcabo@agrofy.com>
2023-03-14 21:01:33 -0300

    Agregado de verificaciones y pruebas que todo ande

--- services/mensajeModal.js
+++ services/mensajeModal.js
@@ -1,5 +1,5 @@
-const myModal = new bootstrap.Modal(document.getElementById('myModal'));
-let nombreCliente = localStorage.getItem('cliente');
+const myModal = new bootstrap.Modal(document.getElementById("myModal"));
+let nombreCliente = localStorage.getItem("cliente");
 
 if (!nombreCliente) {
   myModal.show();
@@ -8,11 +8,11 @@ if (!nombreCliente) {
 }
 //registramos en el local para que no vuleva a preguntar la proxima vez que entramos al sitio.
 function registrarNombre() {
-  let nombre = document.getElementById('nombreCliente');
-  localStorage.setItem('cliente', nombre.value);
+  let nombre = document.getElementById("nombreCliente");
+  localStorage.setItem("cliente", nombre.value);
   escribirNombre(nombre.value);
   myModal.hide();
 }
 function escribirNombre(nombre) {
-  document.getElementById('saludoCliente').append(`${nombre}`);
+  document.getElementById("saludoCliente").append(`${nombre}`);
 }
--- services/traductor.js
+++ services/traductor.js
@@ -1,10 +1,10 @@
-const flagsElement = document.getElementById('flags');
+const flagsElement = document.getElementById("flags");
 
-const textsToChange = document.querySelectorAll('[data-section]');
+const textsToChange = document.querySelectorAll("[data-section]");
 
 const changeLanguage = async (language) => {
   //Fix para detalle de traducción en github
-  const basePart = window.location.href.includes('/mptravesias/') ? '/mptravesias' : '';
+  const basePart = window.location.href.includes("/mptravesias/") ? "/mptravesias" : "";
 
   const requestJson = await fetch(`${basePart}/languages/${language}.json`);
   const texts = await requestJson.json();
@@ -17,7 +17,7 @@ const changeLanguage = async (language) => {
   }
 };
 
-flagsElement.addEventListener('click', (e) => {
+flagsElement.addEventListener("click", (e) => {
   changeLanguage(e.target.parentElement.dataset.language);
 });
 
@@ -26,25 +26,25 @@ flagsElement.addEventListener('click', (e) => {
 //Yo no guardaría un valor por cada idioma, sino que guardaría el idioma que va para después tenerlos.
 //Ej: localStorage.getItem("languageSelected");
 // y que ese valor, me devuelva: eng, esp, bra, etc...
-let idiomaIngles = document.getElementById('in');
-let idiomaEspañol = document.getElementById('es');
+let idiomaIngles = document.getElementById("in");
+let idiomaEspañol = document.getElementById("es");
 
-let ingles = localStorage.getItem('ing');
+let ingles = localStorage.getItem("ing");
 
-if (ingles == 'true') {
-  document.body.classList.add('ingles');
-  changeLanguage('en');
+if (ingles == "true") {
+  document.body.classList.add("ingles");
+  changeLanguage("en");
 } else {
-  document.body.classList.remove('ingles');
-  changeLanguage('es');
+  document.body.classList.remove("ingles");
+  changeLanguage("es");
 }
 
-idiomaIngles.addEventListener('click', () => {
-  document.body.classList.add('ingles');
-  localStorage.setItem('ing', true);
+idiomaIngles.addEventListener("click", () => {
+  document.body.classList.add("ingles");
+  localStorage.setItem("ing", true);
 });
 
-idiomaEspañol.addEventListener('click', () => {
-  document.body.classList.remove('ingles');
-  localStorage.setItem('ing', false);
+idiomaEspañol.addEventListener("click", () => {
+  document.body.classList.remove("ingles");
+  localStorage.setItem("ing", false);
 });
