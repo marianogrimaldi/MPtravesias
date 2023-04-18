a5d546e6c2da152bf989345780b3f3c0e2aa34c3
marianogrimaldi <marianogdanielgrimaldi@gmail.com>
2023-04-18 14:27:26 -0300

    commit 18/04

--- services/imgmarcas.js
+++ services/imgmarcas.js
@@ -1,8 +1,9 @@
 class MarcaVehiculos {
-  constructor(id, name, img) {
+  constructor(id, name, img, url,) {
     this.id = id;
     this.name = name;
     this.img = img;
+    this.url = url;
   }
 }
 
@@ -13,7 +14,7 @@ const cargarMarcas = async () => {
   const datos = await response.json();
   console.log(datos);
   for (let marcaVehiculos of datos) {
-    let marcaNueva = new MarcaVehiculos(marcaVehiculos.id, marcaVehiculos.name, marcaVehiculos.img);
+    let marcaNueva = new MarcaVehiculos(marcaVehiculos.id, marcaVehiculos.name, marcaVehiculos.img, marcaVehiculos.url);
     marcasV.push(marcaNueva);
   }
   let marcas = document.getElementById("marcasVehiculos");
@@ -21,7 +22,7 @@ const cargarMarcas = async () => {
   //Para qué esta segunda iteración, si a esto ya lo podés hacer en el bucle de arriba?
   for (let marca of marcasV) {
     let marcaNueva = document.createElement("div");
-    marcaNueva.innerHTML = `<img src="${marca.img} " alt=" ">`;
+    marcaNueva.innerHTML = `<a href="${marca.url}" target="blank_"><img src="${marca.img} " alt=" " /> </a>`;
     marcas.appendChild(marcaNueva);
   }
 };
