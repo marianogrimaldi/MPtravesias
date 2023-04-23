16ccc88965f8520061262a0a81b6ad493a757725
marianogrimaldi <marianogdanielgrimaldi@gmail.com>
2023-04-23 17:17:19 -0300

    commit final?

--- services/imgmarcas.js
+++ services/imgmarcas.js
@@ -1,9 +1,10 @@
 class MarcaVehiculos {
-  constructor(id, name, img, url,) {
+  constructor(id, name, img, url, principal) {
     this.id = id;
     this.name = name;
     this.img = img;
     this.url = url;
+    this.principal = principal;
   }
 }
 
@@ -14,7 +15,7 @@ const cargarMarcas = async () => {
   const datos = await response.json();
   console.log(datos);
   for (let marcaVehiculos of datos) {
-    let marcaNueva = new MarcaVehiculos(marcaVehiculos.id, marcaVehiculos.name, marcaVehiculos.img, marcaVehiculos.url);
+    let marcaNueva = new MarcaVehiculos(marcaVehiculos.id, marcaVehiculos.name, marcaVehiculos.img, marcaVehiculos.url, marcaVehiculos.principal);
     marcasV.push(marcaNueva);
   }
   let marcas = document.getElementById("marcasVehiculos");
@@ -22,7 +23,9 @@ const cargarMarcas = async () => {
   //Para qué esta segunda iteración, si a esto ya lo podés hacer en el bucle de arriba?
   for (let marca of marcasV) {
     let marcaNueva = document.createElement("div");
-    marcaNueva.innerHTML = `<a href="${marca.url}" target="blank_"><img src="${marca.img} " alt=" " /> </a>`;
+    marcaNueva.innerHTML = `<a href="${marca.url}"  target="blank_"><img src="${marca.img}" ${
+      marca.principal ? "class='principal'" : ""
+    }alt=" " /> </a>`;
     marcas.appendChild(marcaNueva);
   }
 };
